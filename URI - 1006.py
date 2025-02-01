84476660dbbf037c2b913933646fec746a16aace
main <ronaldomont@hotmail.com>
2025-02-01 16:53:27 -0300

    1006 - Média 2

---
+++ URI - 1006.py
@@ -0,0 +1,9 @@
+# Leitura das notas A, B e C
+A = float(input(""))
+B = float(input(""))
+C = float(input(""))
+
+media = (A * 2 + B * 3 + C * 5) / (2 + 3 + 5)
+    
+    # Exibição do resultado
+print(f"MEDIA = {media:.1f}")
