7bb4cd2ef57be1cd1dfaadb6df754a7de54e7624
main <ronaldomont@hotmail.com>
2025-02-04 00:31:18 -0300

    Testando simbolos

--- URI - 1007.py
+++ URI - 1007.py
@@ -3,10 +3,10 @@ Leia quatro valores inteiros A, B, C e D. A seguir,
 calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a 
 fórmula: DIFERENCA = (A * B - C * D).
 
-Entrada
+Entrada>
 O arquivo de entrada contém 4 valores inteiros.
 
-Saída
+Saída>
 Imprima a mensagem DIFERENCA com todas as letras maiúsculas, conforme exemplo abaixo, 
 com um espaço em branco antes e depois da igualdade.
 '''
